b74f8fb5dfc4393acbb97530c805cc333dfd61bd
lunainde <lunainde@gmail.com>
2024-03-18 12:34:52 +0100

    filtered, show/hide w/useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,32 @@ import CourseList from "./components/CourseList";
 import ProjectList from "./components/ProjectList";
 import Results from "./components/Results";
 import MovieList from "./components/MovieList";
+//IMPROVE CONDITIONAL
+import ImprovedMovieList from './components/ImprovedMovieList';
+
+//CONDITIONALS
+import Spinner from './components/Spinner'; 
+import { useState } from 'react';
 
 function App() {
+  //CONDITIONAL
+  //Declare a new state variable, which we'll call "isLoading"
+  // const [isLoading, setIsLoading] = useState(true); 
+  const [isLoading, setIsLoading] = useState(false); 
+  ////IMPROVE CONDITIONAL
+
+    if(isLoading) {
+      return <Spinner/>
+    } else {
+      return (
+        <div className="App">
+          {/* <MovieList /> */}
+          <ImprovedMovieList />
+        </div>
+      )
+    }
+
+
   return (
     <div className="App">
       <SimpleList />
@@ -15,6 +39,7 @@ function App() {
       <ProjectList />
       <Results />
       <MovieList />
+      <ImprovedMovieList />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,14 @@ function MovieCard(props) {
         <h3>{movie.title}</h3>
         <p>Director: {movie.director}</p>
         <p>Rating: {movie.IMDBRating}</p>
+
+        {/* CONDITIONALS */}
+        {/* {movie.hasOscars && <p>Got the Oscar Award! </p>} */}
+        {/* {!movie.hasOscars && <p>Great movie but no Oscars! </p>} */}
         
+        {movie.hasOscars ? <p>Got the Oscar Award! </p> : <p>Great movie but no Oscars!</p>}
+
+        {/* DELETE */}
         <button onClick={() => deleteMovie(movie._id)} className="btn-delete">
           Delete 
         </button>
